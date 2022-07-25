6a9896e2b36e845a19eb55f9be1b29a580b45e4e
Nteman <demosantonis@gmail.com>
2022-07-25 15:28:23 +0300

    Update replace_song.py
    Added basic error handling to Move_to_gd().
    Added overwrite_existing() that will overwrite a song file that has already been replaced before.
    Added dont_overwrite() will remove the mp3 file from the current directory after converting.
    On Rename_existing() added error handling for when a file already exists in gd_song_path and added the option to overwrite it.
    Removed the callback functions from pafy.download() and ffmpeg
    Added basic error handling on main()

--- replace_song.py
+++ replace_song.py
@@ -2,7 +2,7 @@ import pafy #Used for finging and dowloading the desired YouTube videos
 import os, glob #Used for renaming and searching for files
 import subprocess #Used for executing command line commands
 import sys #Used for accessing command line arguements 
-import shutil #Used for moving files between directories
+import shutil #Used for moving files
 from rich.console import Console #Used for customazation of the terminal
 
 console = Console()
@@ -11,31 +11,57 @@ console = Console()
 def Move_to_gd(song_id, current_path, gd_song_path):
     os.chdir(current_path) #Change directory into the current_path
     if f"{song_id}.mp3" in glob.glob("*.mp3"): #Check if the song already exists in the current_path
-        shutil.move(f"{song_id}.mp3", gd_song_path) #Will move it to gd_song_path
+        try:
+            shutil.move(f"{song_id}.mp3", gd_song_path) #Will move it to gd_song_path
 
-        #Print success message
-        console.print(f"MOVED YOUR SONG TO {gd_song_path}, AS: {song_id}.mp3", style="bold green")
-    else: #Will print an error message
-        console.print(f"'{song_id}.mp3' WAS NOT FOUND IN THE CURRENT DIRECTORY: {current_path}", style="bold red")
+            #Print success message
+            console.print(f"Moved your song to: {gd_song_path}", style="bright_black", highlight=False)
+        except shutil.Error as e: #Handle latter
+            print(e)
 
+def overwrite_existing(song_id, gd_song_path):
+    console.print(f"Overwriting...", style="yellow")
     
+    #Remove existing song_id.mp3 file
+    os.remove(f"{gd_song_path}\\{song_id}.mp3")
+    console.print(f"Removed '{song_id}.mp3' from: {gd_song_path}", style="bright_black", highlight=False)
+
+def dont_overwrite(song_id, current_path):
+    console.print(f"Not overwriting...", style="yellow")
+
+    os.remove(f"{current_path}/{song_id}.mp3")
+    console.print(f"Removed {song_id}.mp3 from {current_path}.", style="bright_black", highlight=False)
+
+
+
 #Renames the existing file at "gd_song_path"
-def Rename_existing(song_id, gd_song_path):
+def Rename_existing(song_id, current_path ,gd_song_path):
     try:
         #Will rename the Newgrounds.com song so that it doesn't get lost
         os.rename(f"{gd_song_path}\\{song_id}.mp3", f"{gd_song_path}\\{song_id}-original.mp3")
         
         #Will print simple success message
-        console.print(f"RENAMED THE ORIGINAL FILE AS: {song_id}-original.mp3", style="bold green")
+        console.print(f"Renamed the original file to: {song_id}-original.mp3", style="bright_black", highlight=False)
     except FileExistsError:#Will print simple error message
-        console.print(f"'{song_id}-original.mp3' ALREADY EXISTS IN: {gd_song_path}.", style="bold red")
+        console.print(f"The id: {song_id}, has alrady been replaced before.", style="bold red", highlight=False)
+        console.print(f"Do you want to override the previous replacement? (y/n)", style="bright_black")
+        user_answer = input("")
+        print("\n")
+
+        if user_answer in "yes" or user_answer in "YES":
+            overwrite_existing(song_id, gd_song_path)
+        elif user_answer in "no" or user_answer in "NO":
+            dont_overwrite(song_id, current_path)
+        else:
+            console.print(f"Invalid Input! Enter: y (yes) or n (no).", style="bold red")
+
 
 def Replace(song_id, gd_song_path, current_path):
     os.chdir(gd_song_path) #Change directory into gd_song_path 
 
     #Wil check if file exists in in gd_song_path and act acordingly
     if f"{song_id}.mp3" in glob.glob("*.mp3"):
-        Rename_existing(song_id, gd_song_path)
+        Rename_existing(song_id, current_path, gd_song_path)
         
         Move_to_gd(song_id, current_path, gd_song_path)
     else:#Will just move it
@@ -43,14 +69,15 @@ def Replace(song_id, gd_song_path, current_path):
 
 #Will identify and donwload the song:
 def Download(url, current_path):
-    video = pafy.new(url) #Search for the file
+    video = pafy.new(url, callback=None) #Search for the file
 
     #simple success message
-    console.print(f"FOUND: {video.title}", style="bold green")
+    console.print(f"Found: {video.title}", style="bold green")
 
     audio = video.getbestaudio()#Get the best available audio
     os.chdir(current_path) #Change directory to current_path
     audio.download() #Download the audio
+    print("\n")
 
     return video
 
@@ -60,25 +87,33 @@ def Convert_to_mp3(video, song_id):
 
     # Will use the ffmpeg environment variable in order to convert the dowloaded audio from Download()
     # to an mp3 file that will be recognized by the game 
-    subprocess.run(["ffmpeg", "-i", f"{video.title}.webm", f"{song_id}.mp3"], shell=True) #this code will execute the command
+    subprocess.run(["ffmpeg", "-i", f"{video.title}.webm", f"{song_id}.mp3", "-hide_banner", "-loglevel", "error"], shell=True) #this code will execute the command
 
     #simple success message
-    console.print(f"Converted {video.title}.webm to 'mp3'", style="bold green")
+    console.print(f"Converted: {video.title}.webm to 'mp3'", style="bright_black", highlight=False)
 
 
 def main():
-
     #Variables:
     gd_song_path = r"C:\Users\User\AppData\Local\GeometryDash"
     current_path = os.getcwd()
     url = sys.argv[1] #Desired song
     song_id = sys.argv[2] #Provided song id
+    try:
+        video = Download(url, current_path)
+        Convert_to_mp3(video, song_id)
+        Replace(song_id, gd_song_path, current_path)
 
-    video = Download(url, current_path)
-    Convert_to_mp3(video, song_id)
-    Replace(song_id, gd_song_path, current_path)
-
-    os.remove(video.title + ".webm") #Will delete the initial audio file so that is doesn't take up space
-
+        print("\n")
+        console.print(f"The operation was successful!", style="bold green")
+    except Exception as e:
+        print(e)
+    finally:
+        try:
+            #Will delete the initial audio file so that is doesn't take up space
+            os.remove(video.title + ".webm") 
+        except UnboundLocalError:
+            pass
+        
 if __name__ == '__main__':
     main()
